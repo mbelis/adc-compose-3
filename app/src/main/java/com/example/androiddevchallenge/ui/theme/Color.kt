673e550a78aac5752ad0b3a96e7543e1cbe0d978
Marc Belis <marcbelisvega@gmail.com>
2021-03-13 18:15:29 +0100

    Theme app

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -17,7 +17,9 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val yellow = Color(0xE3DA00)
+val gray900 = Color(0x232323)
+val gray700 = Color(0x2D2D2D)
+val purple = Color(0x322049)
+val green = Color(0x39A844)
+val red = Color(0xd93C19)
